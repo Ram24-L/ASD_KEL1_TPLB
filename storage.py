a10619f43513db1b319b74cb3ef1a510827aa0af
Dapdih <wirawandaffadhiya@apps.ipb.ac.id>
2026-05-13 13:31:33 +0700

    pushing storage

---
+++ storage.py
@@ -0,0 +1,31 @@
+import csv
+import os
+
+FILE_ANTRIAN = 'antrian.csv'
+FILE_LOG = 'log_pelayanan.csv'
+
+def load_data(queue_obj):
+    if os.path.exists(FILE_ANTRIAN):
+        try:
+            with open(FILE_ANTRIAN, mode='r', newline='')as file:
+                reader = csv.DictReader(file)
+                for row in reader:
+                    #masuk ke queue lagi
+                    # enqueue tanpa print
+                    queue_obj.enqueue(row['nama'], row['kategori'])
+                    print(f"[STORAGE] berhasil memuat antrian dari {FILE_ANTRIAN}")
+        except Exception as e:
+            print(f"[ERROR] gagal membaca file antrian: {e}")
+
+            if os.path.exists(FILE_LOG):
+                try:
+                    with open(FILE_LOG, mode='r', newline='') as file:
+                        reader = csv.DictReader(file)
+                        for row in reader:
+                            #membuat objek dummy
+                            from engine import Node
+                            node_log = Node(row['id'], row['nama'], row['kategori'])
+
+                            queue_obj.logs.append(node_log)
+                except Exception as e:
+                    print(f"[ERROR] gagal membaca file log: {e}") 
